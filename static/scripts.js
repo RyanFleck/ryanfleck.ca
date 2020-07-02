797e80eabbb6b5c031ad50af85ed6921c746612d
Ryan Fleck <ryan.fleck@protonmail.com>
2020-07-02 17:47:28 -0400

    Move HTML and CSS to seperate files.

---
+++ static/scripts.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+ * This is the scripts.js file for ryanfleck.ca
+ *
+ * It contains methods for checking the browser storage to apply
+ * user preferences to the css, and controlling the settings modal.
+ *
+ */
+
+window.addEventListener("load", function (event) {
+  // console.log("Hello World!");
+});
